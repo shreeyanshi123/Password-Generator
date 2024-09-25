f02a6a967935c057780078ec55e25741de9bd149
Shree <seemagupta7080185801@gmail.com>
2024-09-25 15:51:00 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { UC, LC, NC, SC } from './PassChar';
 
 function App() {
+
+  const [uppercase, setUpperCase] = useState(false);
+  const [lowercase, setLowerCase] = useState(false);
+  const [numbers, setNumbers] = useState(false);
+  const [symbols, setSymbols] = useState(false);
+  const [passlen, setPasswordLength] = useState(0);
+  const [generatedPassword, setGeneratedPassword] = useState('');
+
+  let createPassword = () => {
+    let finalPas = '';
+    let charSet = '';
+    if (uppercase || lowercase || numbers || symbols) {
+      if (uppercase) {
+        charSet += UC;
+      }
+      if (lowercase) {
+        charSet += LC;
+      }
+      if (numbers) {
+        charSet += NC;
+      }
+      if (symbols) {
+        charSet += SC;
+      }
+
+      for (let i = 0; i < passlen; i++) {
+        finalPas += charSet.charAt(Math.floor(Math.random() * charSet.length));
+      }
+
+      setGeneratedPassword(finalPas);
+
+
+    } else {
+      alert("Please Select onecheckbox");
+    }
+  }
+  const copyToClipboard = () => {
+    if (generatedPassword) {
+      navigator.clipboard.writeText(generatedPassword);
+      alert('Password copied to clipboard!');
+    } else {
+      alert('Please generate a password first.');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="outer-div">
+        <h2>Password Generator</h2>
+        <div className="inner-div1">
+          <input type="text" value={generatedPassword} className="input1" readOnly></input>
+          <button conClick={copyToClipboard}>Copy</button>
+        </div>
+
+        <div className="inner-div2">
+          <label>Password length</label>
+          <input type="number" placeholder="20" className="input2" onChange={(e) => setPasswordLength(e.target.value)}></input>
+        </div>
+        <div className="inner-div2">
+          <label>Include uppercase letters</label>
+          <input type="checkbox" checked={uppercase} onChange={() => setUpperCase(!uppercase)} className="check"></input>
+        </div>
+
+        <div className="inner-div2">
+          <label>Include lowercase letters</label>
+          <input type="checkbox" checked={lowercase} onChange={() => setLowerCase(!lowercase)} className="check"></input>
+        </div>
+        <div className="inner-div2">
+          <label>Include numbers</label>
+          <input type="checkbox" checked={numbers} onChange={() => setNumbers(!numbers)} className="check"></input>
+        </div>
+        <div className="inner-div2">
+          <label>Include symbols</label>
+          <input type="checkbox" checked={symbols} onChange={() => setSymbols(!symbols)} className="check"></input>
+        </div>
+        <button className="btn" onClick={createPassword}>Generate Password</button>
+      </div>
+
+
+    </>
   );
 }
 
